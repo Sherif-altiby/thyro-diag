6522fe9ad3046b8a4d99b9658ce49a8970ef9445
Sherif Altiby <sherifahmed852002@gmail.com>
2026-02-27 23:45:04 +0200

    add vercel settings

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import app from './src/app.js';
-import { connectDB } from './src/config/db.js';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+import app        from './src/app.js';
+import connectDB  from './src/config/db.js';  // 👈 default import, no {}
 
 connectDB().then(() => {
   app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
   });
 });
+
+export default app; // 👈 required for Vercel
